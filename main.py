9469c3e323f5155ca5f82fefb2a1d652bdc86a33
AlexPMontes <100499978@alumnos.uc3m.es>
2025-02-07 22:31:28 +0100

    feat: added loop in main to execute the code with two json, one with valid iban and the other with invalid iban

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import string
 #GLOBAL VARIABLES
 letters = string.ascii_letters + string.punctuation + string.digits
 shift = 3
-
+test_files = ["valid_test.json", "invalid_test.json"]
 
 def Encode(word):
     encoded = ""
@@ -28,17 +28,17 @@ def Decode(word):
 
 
 def main():
-
-    mng = TransactionManager()
-    res = mng.ReadproductcodefromJSON("test.json")
-    strRes = res.__str__()
-    print(strRes)
-    EncodeRes = Encode(strRes)
-    print("Encoded Res "+ EncodeRes)
-    DecodeRes = Decode(EncodeRes)
-    print("Decoded Res: " + DecodeRes)
-    print("IBAN_FROM: " + res.IBAN_FROM)
-    print("IBAN_TO: " + res.IBAN_TO)
+    for i in range(2):
+        mng = TransactionManager()
+        res = mng.ReadproductcodefromJSON(test_files[i])
+        strRes = res.__str__()
+        print(strRes)
+        EncodeRes = Encode(strRes)
+        print("Encoded Res "+ EncodeRes)
+        DecodeRes = Decode(EncodeRes)
+        print("Decoded Res: " + DecodeRes)
+        print("IBAN_FROM: " + res.IBAN_FROM)
+        print("IBAN_TO: " + res.IBAN_TO)
 
 if __name__ == "__main__":
     main()
